ecdab033bd9084288ed358c3cf878bf8710f1484
MatiasGarrido <gcmatos24@gmail.com>
2022-08-07 06:31:19 -0400

    Agregado efecto al navbar al momento de hacer scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,19 @@ $(function () {
 $("#enviar").click(function () {
     alert("¡Gracias por ponerte en contacto con Viajes Chile! Tu mensaje ha sido enviado correctamente.")
   });  
+
   
+  $(document).scroll(function(){
+    const y = $("html").scrollTop();
+
+    y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
+  })
+
 
 })
+
+$(document).scroll(function () {
+    const y = $("html").scrollTop();
+  
+    y > 300 ? $(".navbar").addClass("nav-black") : $(".navbar").removeClass("nav-black");
+  });
