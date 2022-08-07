03648b7d85d08e239970b4bc4da9ffd07d55972d
MatiasGarrido <gcmatos24@gmail.com>
2022-08-07 05:47:57 -0400

    Agregados tooltips al footer y alert al boton de contacto

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  
+  
+  
+$("#enviar").click(function () {
+    alert("¡Gracias por ponerte en contacto con Viajes Chile! Tu mensaje ha sido enviado correctamente.")
+  });  
+  
+
+})
